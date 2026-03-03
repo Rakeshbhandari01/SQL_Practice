192773feb30432d58ab825c31c0c3118bce7703b
Rakeshbhandari01 <rakeshbhandari772@gmail.com>
2026-03-03 16:29:41 +0530

    Create database

---
+++ Day2.sql
@@ -0,0 +1,15 @@
+-- Create database
+create database Student;
+
+-- Use Database
+use student;
+
+-- Create table 
+create table stud(name varchar(100), id int, address varchar(100));
+desc stud;
+
+-- insert command
+insert into stud values("Akash",01,"Ganga Nagar");
+
+-- Retrieve all data 
+Select * from stud;
